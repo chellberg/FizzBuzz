c65bc2b85964fb530a7199b2879324551bca0d11
Chellberg <corinchellberg@gmail.com>
2013-07-07 16:04:56 -0400

    HEY GUYS

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,31 @@
 # FizzBuzz interview question
+class FizzBuzz
 
-(1..100).each do |i|
-  if i % 3 == 0 and i % 5 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0 
-    puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  else 
-    puts i
+  @@output = Array.new(100) #initilize empty array
+
+  def five_three #fill array with FizzBuzz
+    
+   @@output.each do |i|
+      if i % 3 == 0 and i % 5 == 0
+        output[i] = "FizzBuzz"
+      elsif i % 3 == 0 
+        output[i] = "Fizz"
+      elsif i % 5 == 0
+        output[i] = "Buzz"
+      else 
+        output[i] = i
+      end
+    end
   end
+
+  def printer()
+    puts @@output
+  end
+ 
+ 
 end
+
+a = FizzBuzz.new
+a.five_three
+a.printer
+
