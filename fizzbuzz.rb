198e0477a7ef32f4977c86b17941ce21327a8235
Chellberg <corinchellberg@gmail.com>
2013-07-07 15:03:10 -0400

    Remove unnecessary elsif checks

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 (1..100).each do |i|
   if i % 3 == 0 and i % 5 == 0
     puts "FizzBuzz"
-  elsif i % 3 == 0 and i % 5 != 0 
+  elsif i % 3 == 0 
     puts "Fizz"
-  elsif i % 5 == 0 and i % 3 != 0
+  elsif i % 5 == 0
     puts "Buzz"
   else 
     puts i
